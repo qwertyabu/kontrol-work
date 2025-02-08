afcfc49074ea5cd854772b4c8ead0d0244155c55
qwertyabu <monkey.famil4@gmail.com>
2025-02-08 16:08:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const name = prompt("Ismingiz nima?");
+const age = prompt("yoshingiz nechida");
+
+document.querySelector("#name").textContent = name;
+document.querySelector("#age").textContent = age;
